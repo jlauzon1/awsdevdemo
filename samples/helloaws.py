4ec93ffc13184a179061c08f1165e8b8665225cf
jlauzon1 <118931345+jlauzon1@users.noreply.github.com>
2022-11-23 13:13:27 -0600

    Update helloaws.py

--- samples/helloaws.py
+++ samples/helloaws.py
@@ -13,7 +13,7 @@ for bucket in s3.buckets.all():
 print(buffer)
 
 # Publish the bucket names to SNS topic
-topic = sns.Topic("arn:aws:sns:us-west-2:691995883950:DevTopic")
+topic = sns.Topic("arn:aws:sns:us-east-1:114038307870:DevTopic:DevTopic")
 response = topic.publish(
         Message="My buckets: " + buffer
     )
